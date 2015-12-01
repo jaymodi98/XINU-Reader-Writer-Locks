1c14378234bb290405b06c5b20774cbf8343105f
mnsekh111 <mnsekh1111@gmail.com>
2015-12-01 18:28:45 -0500

    Test case 0 Passed - Up and running

--- sys/ldelete.c
+++ sys/ldelete.c
@@ -5,4 +5,40 @@
  *      Author: mns
  */
 
+#include <kernel.h>
+#include <proc.h>
+#include <q.h>
+#include <lock.h>
+#include <stdio.h>
+
+/*------------------------------------------------------------------------
+ * ldelete  --  delete a lock by releasing its table entry
+ *------------------------------------------------------------------------
+ */
+
+int ldelete(int ldesc){
+	STATWORD ps;
+	int pid;
+	struct lentry *lptr;
+
+	disable(ps);
+	if(isbadlock(ldesc) || locks[ldesc].lstate == LFREE){
+		restore(ps);
+		return SYSERR;
+	}
+
+	lptr = &locks[ldesc];
+	lptr->lstate = LFREE;
+
+	if(nonempty(lptr->lqhead)){
+		while((pid=getfirst(lptr->lqhead)) != EMPTY){
+			proctab[pid].pwaitret = DELETED;
+			ready(pid,RESCHNO);
+		}
+		resched();
+	}
+
+	restore(ps);
+	return (OK);
+}
 
--- sys/linit.c
+++ sys/linit.c
@@ -11,16 +11,23 @@
 #include <proc.h>
 
 struct lentry locks[NLOCK];
+int locktab[NPROC][NLOCK];
 int nextlock;
 
 void linit() {
 
 	struct lentry * lptr;
-	int i;
+	int i, j;
 	nextlock = NLOCK - 1;
 
 	for (i = 0; i < NLOCK; i++) { /* initialize semaphores */
 		(lptr = &locks[i])->lstate = LFREE;
 		lptr->lqtail = 1 + (lptr->lqhead = newqueue());
+	}
+
+	for (i = 0; i < NPROC; i++) {
+		for (j = 0; j < NLOCK; j++) {
+			locktab[i][j] = NOTUSING;
+		}
 	}
 }
